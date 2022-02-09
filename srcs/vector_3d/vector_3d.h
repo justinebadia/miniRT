0a9e535db2f4591864dae78f4efd2b9f94202d84
Tokutaro Shimoda <tshimoda@c3r5p1.42quebec.com>
2022-02-09 15:37:06 -0500

    vector header

--- srcs/vector_3d/vector_3d.h
+++ srcs/vector_3d/vector_3d.h
@@ -6,7 +6,7 @@
 /*   By: tshimoda <tshimoda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/06 16:30:59 by tshimoda          #+#    #+#             */
-/*   Updated: 2022/02/09 14:55:34 by tshimoda         ###   ########.fr       */
+/*   Updated: 2022/02/09 15:35:09 by tshimoda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,14 @@ typedef struct s_vector
 	double	z;
 }	t_vector;
 
+t_vector	new_vector(double x, double y, double z);
 t_vector	new_vec_add(t_vector u, t_vector v);
 t_vector	new_vec_substract(t_vector u, t_vector v);
 t_vector	new_vec_multiply(t_vector u, double scalar);
 t_vector	vector_normalize(t_vector u);
+t_vector	new_vec_crossproduct(t_vector u, t_vector v);
+void		mod_vec_normalize(t_vector *v);
+double		get_vector_length(t_vector v);
+double		get_dot_product(t_vector u, t_vector v);
+
 #endif
